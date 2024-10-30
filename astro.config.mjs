f8ed27b6436aa4da86f3061cfa67656ab9e9d700
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-10-30 12:00:52 +0100

    build: setup to publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import compress from 'astro-compress';
 
 import sitemap from '@astrojs/sitemap';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://simoneferretti.dev',
-	integrations: [compress(), sitemap()],
-});
+  site: 'https://simoneferretti.dev',
+  integrations: [compress(), sitemap()],
+  output: 'server',
 
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
